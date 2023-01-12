5007bb43014b9f39029052eb1cafdbaf7266dad8
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-01-12 14:19:10 +0900

    Update nvm setting

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -3,3 +3,15 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 fi
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+INSTALLED_VERSION=$(nvm ls 16)
+if grep -q "16" <<< "$INSTALLED_VERSION";
+then
+    echo "Use node 16"
+else
+    echo "Install node 16"
+    nvm install 16
+fi
+nvm use 16
