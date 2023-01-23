6d06285085423b8c008c6cb49a878322cb7b52eb
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-01-23 16:41:08 +0900

    Refactoring rc

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,4 +1,4 @@
-source ~/.zsh/antigen.zsh
+source "$HOME/.zsh/antigen.zsh"
 
 antigen use oh-my-zsh
 antigen bundle git
@@ -13,8 +13,8 @@ HISTSIZE=10000000
 HISTFILESIZE=10000000
 
 h=()
-if [[ -r ~/.ssh/config ]]; then
-   h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+if [[ -r "$HOME/.ssh/config" ]]; then
+   h=($h ${${${(@M)${(f)"$(cat $HOME/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
 fi
 if [[ $#h -gt 0 ]]; then
    zstyle ':completion:*:ssh:*' hosts $h
@@ -24,47 +24,37 @@ setopt nonomatch
 
 setopt NO_HUP
 
-source ~/.cargo/env
-
-if [ $(uname -s) = Darwin ]; then
-    eval $(/opt/homebrew/bin/brew shellenv)
-else
-    source /etc/zsh_command_not_found
-fi
-
-
-[[ ! -f ~/.zsh/p10k.zsh ]] || source ~/.zsh/p10k.zsh
-[[ ! -f ~/.zsh/ocaml.zsh ]] || source ~/.zsh/ocaml.zsh
-[[ ! -f ~/.zsh/hook.zsh ]] || source ~/.zsh/hook.zsh
-[[ ! -f ~/.config/z/z.sh ]] || source ~/.config/z/z.sh
-[[ ! -f ~/.fzf.zsh ]] || source ~/.fzf.zsh
-[[ ! -f ~/.zsh/nvm.zsh ]] || source ~/.zsh/nvm.zsh
-
 # conditional rtags
-if [ -d ~/.dotfiles/rtags ]; then
-    export PATH=$PATH:~/.dotfiles/rtags/bin
+if [ -d "$HOME/.dotfiles/rtags" ]; then
+    export PATH=$PATH:"$HOME/.dotfiles/rtags/bin"
 fi
 
+# PATH
 export GEM_HOME="$HOME/.gems"
-export PATH="$HOME/.gems/bin:$PATH"
+export PATH="$HOME/.gems/bin":"$HOME/.local/bin":"$HOME/.emacs.d/bin":"$HOME/.rbenv/bin":$PATH
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$HOME/google-cloud-sdk/path.zsh.inc"]; then . "$HOME/google-cloud-sdk/path.zsh.inc" fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc"]; then . "$HOME/google-cloud-sdk/completion.zsh.inc" fi
-
-[ -d "$HOME/.local/bin" ] && export PATH="$PATH:$HOME/.local/bin"
-export PATH=$PATH:"$HOME/.emacs.d/bin"
+[[ ! -f "$HOME/google-cloud-sdk/path.zsh.inc" ]] || . "$HOME/google-cloud-sdk/path.zsh.inc"
+[[ ! -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]] || . "$HOME/google-cloud-sdk/completion.zsh.inc"
 
 if [[ $OSTYPE == 'darwin'* ]]; then
+    eval $(/opt/homebrew/bin/brew shellenv)
     export PATH="/opt/homebrew/bin:$PATH"
     export LDFLAGS="-L/opt/homebrew/lib"
     export CPPFLAGS="-I/opt/homebrew/include"
     export CXXFLAGS="-I/opt/homebrew/include"
+else
+    source /etc/zsh_command_not_found
 fi
 
-[[ ! -f ~/.zsh/alias.zsh ]] || source ~/.zsh/alias.zsh
+[[ ! -f "$HOME/.cargo/env" ]] || source "$HOME/.cargo/env"
+[[ ! -f "$HOME/.zsh/p10k.zsh" ]] || source "$HOME/.zsh/p10k.zsh"
+[[ ! -f "$HOME/.zsh/ocaml.zsh" ]] || source "$HOME/.zsh/ocaml.zsh"
+[[ ! -f "$HOME/.zsh/hook.zsh" ]] || source "$HOME/.zsh/hook.zsh"
+[[ ! -f "$HOME/.config/z/z.sh" ]] || source "$HOME/.config/z/z.sh"
+[[ ! -f "$HOME/.fzf.zsh" ]] || source "$HOME/.fzf.zsh"
+[[ ! -f "$HOME/.zsh/nvm.zsh" ]] || source "$HOME/.zsh/nvm.zsh"
 
-export PATH="$HOME/.rbenv/bin":$PATH
-eval "$(rbenv init -)"
+# load alias finally
+[[ ! -f "$HOME/.zsh/alias.zsh" ]] || source "$HOME/.zsh/alias.zsh"
+
+eval "$(rbenv init - zsh)"
