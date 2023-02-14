504f8b0b59fed475ac16128d301a7b3fb98f3402
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-02-14 18:06:04 +0900

    Add zsh plugin

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -6,6 +6,8 @@ antigen bundle pip
 antigen bundle docker
 antigen bundle command-not-found
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
 antigen theme romkatv/powerlevel10k
 antigen apply
 
