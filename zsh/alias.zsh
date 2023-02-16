50a8ee3f00d1232b7932ac98dfb0ea9b2d70a906
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2023-02-16 10:45:21 +0900

    Add more guard for alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,6 +1,6 @@
 alias count='wc -l'
 alias dist="cat /etc/*-release | grep PRETTY | awk -F = '{print $2}'"
-alias vi='nvim'
+[[ -x $(which nvim) ]] && alias vi='nvim'
 alias grep='grep --color=auto'
 alias png='dot -Tpng -O'
 alias dotclear='rm -rf *.dot *.png'
@@ -9,5 +9,5 @@ alias ssh-hosts="grep -P \"^Host ([^*]+)$\" $HOME/.ssh/config | sed 's/Host //'"
 alias jsonify='python -m json.tool'
 [[ -x $(which cargo) ]] && alias ls='exa'
 [[ -s $(which nemo) ]] && alias here='nemo .'
-alias caml='eval $(opam env)'
-alias o='opam'
+[[ -x $(which opam) ]] && alias caml='eval $(opam env)'
+[[ -x $(which opam) ]] && alias o='opam'
