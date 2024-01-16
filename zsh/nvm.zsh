2b8bb6adf862a1c18d40770b036fb588807cc35d
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2024-01-16 18:18:35 +0900

    comment out force nvm use 16

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -6,19 +6,19 @@ fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-INSTALLED_VERSION=$(nvm ls 16)
-if grep -q "16" <<< "$INSTALLED_VERSION";
-then
-    echo "Node 16 is already installed"
-else
-    echo "Install node 16"
-    nvm install 16
-fi
-CURRENT_VERSION=$(nvm current)
-if grep -q "16" <<< "$CURRENT_VERSION";
-then
-    echo "Node 16 is already set as current"
-else
-    echo "Set node 16 as current"
-    nvm use 16
-fi
+# INSTALLED_VERSION=$(nvm ls 16)
+# if grep -q "16" <<< "$INSTALLED_VERSION";
+# then
+#     echo "Node 16 is already installed"
+# else
+#     echo "Install node 16"
+#     nvm install 16
+# fi
+# CURRENT_VERSION=$(nvm current)
+# if grep -q "16" <<< "$CURRENT_VERSION";
+# then
+#     echo "Node 16 is already set as current"
+# else
+#     echo "Set node 16 as current"
+#     nvm use 16
+# fi
