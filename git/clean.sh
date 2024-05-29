8e45a4124f1b6e50822ebcd9b87b703af261d19e
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2024-05-29 16:17:21 +0900

    fix git gc logic -- handle all remotes

--- git/clean.sh
+++ git/clean.sh
@@ -3,8 +3,8 @@
 git checkout $(git default)
 TMP_HEADS=$(mktemp /tmp/HEADS.XXXXXX)
 TMP_REMOTES=$(mktemp /tmp/REMOTES.XXXXXX)
-git for-each-ref refs/heads/* --format='%(refname:lstrip=2)' >> "$TMP_HEADS"
-git for-each-ref refs/remotes/origin/* --format='%(refname:lstrip=3)' >> "$TMP_REMOTES"
+git for-each-ref refs/heads/ --format='%(refname:lstrip=2)' >> "$TMP_HEADS"
+git for-each-ref refs/remotes/ --format='%(refname:lstrip=3)' >> "$TMP_REMOTES"
 CANDIDATES=$(comm -23 "$TMP_HEADS" "$TMP_REMOTES")
 for ref in $CANDIDATES; do
     read -r -p "Are you sure to delete branch $ref? [Y/n]" answer
